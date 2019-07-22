eb8e02086f8c31eda4b43a98c21a49268f0f770d
Faiz Fanani <fanani_pandemonium@yahoo.co.id>
2019-07-22 11:24:26 +0700

    submission 2

--- app/src/main/java/com/kontrakanelite/movieapp/activity/MainActivity.java
+++ app/src/main/java/com/kontrakanelite/movieapp/activity/MainActivity.java
@@ -1,27 +1,46 @@
 package com.kontrakanelite.movieapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.kontrakanelite.movieapp.adapter.TabAdapter;
 import com.kontrakanelite.movieapp.R;
 
 public class MainActivity extends AppCompatActivity {
     public TabAdapter adapter;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = findViewById(R.id.viewPager);
-        tabLayout = findViewById(R.id.tabLayout);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
         adapter = new TabAdapter(getSupportFragmentManager());
         adapter.addFragment(new movieList(), "Movies");
         adapter.addFragment(new tvShowList(), "TV Shows");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_settings){
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
